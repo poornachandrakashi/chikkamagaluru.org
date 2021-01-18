a0d4597062b9f84d8b6d80f6d2599243a3ed037c
Sanket Patil <sanket.9006@gmail.com>
2021-01-18 23:03:27 +0530

    stylesheet + Volunteer Registration

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,8 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer">
-      <footer class="py-5 bg-dark fixed-bottom">
+       {/* fixed-bottom */}
+      <footer class="py-5 bg-dark">
         <div class="container">
           <p class="m-0 text-center text-white">
             &copy; {new Date().getFullYear()} Copyright : <a href="http://chikkamagaluru.org/"> chikkamagaluru.org </a>
