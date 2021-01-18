73060a76e2ca620621dcf906b8f5961aeba67fdc
Sanket Patil <sanket.9006@gmail.com>
2021-01-18 17:29:10 +0530

    React Router - DOM

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import './App.css';
 
+// Importing Components
+import Navigation  from './components/Navigation/Navigation'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Preview Environment for chikkamagaluru.org website</p>
-        <a
-          className="App-link"
-          href="https://chikkamagaluru.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          https://chikkamagaluru.org/ 
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route path="/" exact component={() => <Home />} />
+        <Route path="/about" exact component={() => <About />} />
+        <Route path="/contact" exact component={() => <Contact />} />
+        <Redirect from="*" to="/" />
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
+
+
 
 export default App;
