b0702f8b381be6759d99bf710572befa2c0bd5a0
Sanket Patil <sanket.9006@gmail.com>
2021-01-19 20:04:23 +0530

    Responsive BG Image + About Coffee

---
+++ src/components/Home/Hero Section/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'react-bootstrap/Image'
+import { Container, Row } from 'react-bootstrap';
+import desktopImage from '../../../Images-Videos-GIF/chikkamagaluru-desktop.jpg';
+import mobileImage from '../../../Images-Videos-GIF/chikkamagaluru-mobile.jpg';
+import './Hero.css';
+
+const Hero = () => {
+    const imageUrl = window.innerWidth >= 1081 ? desktopImage : mobileImage;
+
+    return (
+        <div className="Hero" style={{ backgroundImage: `url(${imageUrl})` }}>
+             <div className="Hero-content">
+            <h2>asfs</h2>
+        </div>
+        </div>
+
+    )
+}
+
+export default Hero
