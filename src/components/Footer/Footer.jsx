86c8cf3455e895b237f81a6e407c5b34e1842c7e
Akhil-chaudhary <91989976a@gmail.com>
2021-01-20 15:28:56 +0530

    Added css file and base struct in footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,81 @@
 import React from "react";
+import "./Footer.css";
+import { AiFillFacebook,AiOutlineTwitter,AiFillInstagram } from "react-icons/ai";
 
 function Footer() {
   return (
     <div className="footer">
-       {/* fixed-bottom */}
-      <footer class="py-5 bg-dark">
-        <div class="container">
-          <p class="m-0 text-center text-white">
-            &copy; {new Date().getFullYear()} Copyright : <a href="http://chikkamagaluru.org/"> chikkamagaluru.org </a>
+      {/* fixed-bottom */}
+      <footer className="py-5 bg-dark">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-2 col-md-2 item">
+              <h3>Things to do</h3>
+              <ul>
+                <li>
+                  <a href="#">A</a>
+                </li>
+                <li>
+                  <a href="#">B</a>
+                </li>
+                <li>
+                  <a href="#">C</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-sm-2 col-md-2 item">
+              <h3>Things to know</h3>
+              <ul>
+                <li>
+                  <a href="#">A</a>
+                </li>
+                <li>
+                  <a href="#">B</a>
+                </li>
+                <li>
+                  <a href="#">C</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-sm-2 col-md-2 item">
+              <h3>Community</h3>
+              <ul>
+                <li>
+                  <a href="#">A</a>
+                </li>
+                <li>
+                  <a href="#">B</a>
+                </li>
+                <li>
+                  <a href="#">C</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-6 item text">
+              <h3>Chikkamagaluru</h3>
+              <p>
+                Located at the foothills of the Mullayanagiri range, and just
+                south of Baba Budangiris, Chikmagalur is famous for its forests,
+                mountains wildlife and in particular peacocks. Widely known as
+                the “coffee land” of Karnataka, it is one of the largest
+                producers of coffee in India.
+              </p>
+            </div>
+            <div className="col item social">
+              <a href="#">
+                <AiFillFacebook/>
+              </a>
+              <a href="#">
+                <AiOutlineTwitter/>
+              </a>
+              <a href="#">
+                <AiFillInstagram/>
+              </a>
+            </div>
+          </div>
+          <p className="m-0 mt-4 text-center text-white">
+            &copy; {new Date().getFullYear()} Copyright :{" "}
+            <a href="http://chikkamagaluru.org/"> chikkamagaluru.org </a>
           </p>
         </div>
       </footer>
