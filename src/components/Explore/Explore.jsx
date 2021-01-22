c7bb79c6a6e479c2889fd71e848042072e9658fd
Sanket Patil <sanket.9006@gmail.com>
2021-01-22 16:45:44 +0530

    Added Map into Explore Page (Section)

--- src/components/Explore/Explore.jsx
+++ src/components/Explore/Explore.jsx
@@ -1,29 +1,17 @@
 import React from "react";
+import Map from '../../shared-components/Map/Map'
+import { Container, Col, Row } from 'react-bootstrap'
 
 function Explore() {
   return (
-    <div className="about">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="https://chikkamagaluru.org/static/media/chikkamagaluru-desktop.dcc573e2.jpg"
-              alt=""
-            />
-          </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Explore</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Map />
+        </Col>
+      </Row>
+
+    </Container>
   );
 }
 
