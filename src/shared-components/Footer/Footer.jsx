544134207f160ccd78c19311bd1af8f910f861f1
Akhil-chaudhary <91989976a@gmail.com>
2021-01-22 22:16:44 +0530

    Added a variables file named index

--- src/shared-components/Footer/Footer.jsx
+++ src/shared-components/Footer/Footer.jsx
@@ -6,56 +6,31 @@ import {
   AiOutlineTwitter,
   AiFillInstagram,
 } from "react-icons/ai";
+import { index } from "../../variables/content";
 
 function Footer() {
+  function navListItemsRender(head){
+    return head.map((items)=>(
+        <li>
+          <a href="https://chikkamagaluru.org/">{items}</a>
+        </li>
+    ));
+  }
+  const navListHeads = index.footer.navigation.map((nav) => (
+    <div className="col-sm-2 col-md-2 item">
+      <h3>{nav.head}</h3>
+      <ul>
+        {navListItemsRender(nav.items)}
+      </ul>
+    </div>
+  ));
   return (
     <div className="footer">
       {/* fixed-bottom */}
       <footer className="py-5 bg-dark">
         <div className="container">
           <div className="row">
-            <div className="col-sm-2 col-md-2 item">
-              <h3>Things to do</h3>
-              <ul>
-                <li>
-                  <a href="https://chikkamagaluru.org/">A</a>
-                </li>
-                <li>
-                  <a href="https://chikkamagaluru.org/">B</a>
-                </li>
-                <li>
-                  <a href="https://chikkamagaluru.org/">C</a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-sm-2 col-md-2 item">
-              <h3>Things to know</h3>
-              <ul>
-                <li>
-                  <a href="https://chikkamagaluru.org/">A</a>
-                </li>
-                <li>
-                  <a href="https://chikkamagaluru.org/">B</a>
-                </li>
-                <li>
-                  <a href="https://chikkamagaluru.org/">C</a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-sm-2 col-md-2 item">
-              <h3>Community</h3>
-              <ul>
-                <li>
-                  <a href="https://chikkamagaluru.org/">A</a>
-                </li>
-                <li>
-                  <a href="https://chikkamagaluru.org/">B</a>
-                </li>
-                <li>
-                  <a href="https://chikkamagaluru.org/">C</a>
-                </li>
-              </ul>
-            </div>
+            {navListHeads}
             <div className="col-md-6 item text">
               <h3>
                 <img
@@ -69,11 +44,7 @@ function Footer() {
                 <span>Chikkamagaluru, Karnataka</span>
               </h3>
               <p>
-                Located at the foothills of the Mullayanagiri range, and just
-                south of Baba Budangiris, Chikmagalur is famous for its forests,
-                mountains wildlife and in particular peacocks. Widely known as
-                the “coffee land” of Karnataka, it is one of the largest
-                producers of coffee in India.
+                {index.footer.description}
               </p>
             </div>
             <div className="col item social">
---
+++ src/variables/content.js
@@ -0,0 +1,26 @@
+let index = {
+  footer: {
+    navigation: [
+      {
+        head: "Things to do",
+        items: ["Arts and culture", "Family fun", "Treking"],
+      },
+      {
+        head: "Things to know",
+        items: ["Top Five Featured", "Areas of Interest", "Travel guides"],
+      },
+      { head: "Community", items: ["Blogs", "Photos"] },
+    ],
+    description:
+      "Located at the foothills of the Mullayanagiri range, and just south of Baba Budangiris, Chikmagalur is famous for its forests, mountains wildlife and in particular peacocks. Widely known as the “coffee land” of Karnataka, it is one of the largest producers of coffee in India.",
+    "social-links": {
+      facebook: "",
+      twitter: "",
+      instagram: "",
+    },
+  },
+};
+
+module.exports = {
+  index,
+};
